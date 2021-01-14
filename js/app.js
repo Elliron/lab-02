c0ddad5a0323d54b53d0ce173e29da2a1d250491
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-01-13 17:06:15 -0800

    debugged so that dropdown list rendered properly and event handler fired properly

--- js/app.js
+++ js/app.js
@@ -24,113 +24,69 @@ function Animals(name, url, description, keyword, horns) {
 
 // Renders to page
 Animals.prototype.render = function () {
-  // console.log(this);
-  
-  // const $dropList = $('option:first-child').clone();
-  // $dropList.
-  const tempVar = $('#photo-template').html();
-  const $animalRender = $(`<section>${tempVar}</section>`);
+
+  const photoTemplate = $('#photo-template').html();
+  const $animalRender = $(`<section>${photoTemplate}</section>`);
+  $animalRender.addClass(`${this.keyword}`);
   const $h2 = $animalRender.find('h2');
+
   $h2.text(this.title);
-  
+
   const $img = $animalRender.find('img');
   $img.attr('src', this.image_url);
-  // console.log('src', this.image_url);
   $img.attr('alt', this.title);
   const $description = $animalRender.find('p');
   $description.text(this.description);
-  
+
   $('main').append($animalRender);
-  
+
 };
-
-// Dog.prototype.render = function(){
-  //   const $dogClone2 = $('li:first-child').clone(); // only get the first li
-  //   // Goal: target the h2 of the cloned li
-  //   // getter method .find();
-  //   // `.find()` === `$()` except it only looks inside the object we call it on
-//   const $h2 = $dogClone2.find('h2');
-//   $h2.text(this.name);
-
-//   const $img = $dogClone2.find('img');
-//   $img.attr('src', this.image_url);
-//   $img.attr('alt', this.hobbies);
-
-//   $dogClone2.find('p').text(this.hobbies);
-
-//   $('ul').append($dogClone2);
-// };
-
-/* <main>
-<template id="photo-template">
-  <h2></h2>
-  <img src="" alt="">
-  <p></p>
-</template>
-</main> */
 
 // ajax retrieves objects from json file and puts it in new array, calls render function
 $.ajax('/data/page-1.json').then(stuffThatComesBack => {
-  // console.log(stuffThatComesBack);
+
   stuffThatComesBack.forEach((animal) => {
     Animals.animalsArray.push(new Animals(animal.title, animal.image_url, animal.description, animal.keyword, animal.horns));
   });
   console.log(Animals.animalsArray);
+
   //New animal constructor complete, code is finished, next step is Animals.animalsArray that we made loops through and calls render function at each instance of loop
-  Animals.animalsArray.forEach((animalImg) => {
-    console.log('animal image', animalImg.title);
-    // const dropList = $('select').html();
-    // const $listRender = $(`<select>${dropList}</select>`);
-    // const $option = $listRender.find('option');
-    // $option.attr('value', Animals.animalsArray[animalImg].keyword);
-    animalImg.render();
+  const filterKeyword = [];
+
+  Animals.animalsArray.forEach((animal) => {
+
+    // populate filter
+    if (!filterKeyword.includes(animal.keyword)) {
+      const $option = $(`<option>${animal.keyword}</option>`);
+      $option.attr('value', animal.keyword);
+      $('select').append($option);
+      filterKeyword.push(animal.keyword);
+    }
+
+    // console.log(animal);
+    animal.render();
+
   });
+});
 
-
-})
 // .then(Animals.populateFilter);
 
 Animals.populateFilter = () => {
-  const filterKeyword = [];
   console.log('populateFilter');
+  const filterKeyword = [];
+
   // for each loop animals array - inside of it, push the keyword into the filterKeyword array
   Animals.animalsArray.forEach((animalImg) => {
     filterKeyword.push(animalImg.keyword);
   });
-
-  filterKeyword.forEach((keyword) => {
-    const dropList = $('option').clone()[0];
-    const $listRender = $(`<select>${dropList}</select>`);
-    const $option = $listRender.find('option');
-    $option.attr('value', this.keyword);
-  });
-  // for (let i = 0; i < Animals.animalsArray.length; i++) {
-  //   const dropList = $('option').clone()[0];
-  //   const $listRender = $(`<select>${dropList}</select>`);
-  //   const $option = $listRender.find('option');
-  //   $option.attr('value', Animals.animalsArray.keyword);
-  // }
-
-  // for each over the filterKeyword array; for each of those keywords, create an <option> tag and dynamically add the value using a template literal
 };
 
-  // // Clicking and 'filtering'
-  // $('button').on('click', () => { // In today's lab you need to filter only the images with the specific keyword
-  //   //goal is to only show The Clifford li
+$('select').on('change', (event) => {
+  // event.preventDefault();
+  $('section').hide();
+  let userValue = event.target.value;
+  console.log(userValue);
 
-  //   // get rid of the other ones
-  //   // iterate and check if the name is clifford
-  //   // if the name is clifford : render
-
-  //   // OR
-
-  //   // target all the list items and hide them
-  //   // target the one with an h2 with text of 'clifford' and show it
-  //   $('li').hide();
-  //   $('li:contains(Clifford)').show();
-
-  // });
-
-  // $('select').on('click', () => {
-
-// });
+  // documentation: Sara Russert showed us how she targeted a section by class
+  $(`section[class=${userValue}]`).show();
+});
